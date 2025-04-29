6c2b6c32da35f9b2cd06ccd5117e5a4981622a8b
quocbao1327 <tranquocbao20022011@gmail.com>
2025-04-29 23:48:24 +0700

    FIX

--- LENH IF ELSE VA BANG MA ASCII/BAI 9.cpp
+++ LENH IF ELSE VA BANG MA ASCII/BAI 9.cpp
@@ -2,10 +2,11 @@
 
 int main(){
 	int n;
-	long long Sum;
-	scanf ( "%d" , &n );
-	Sum = ( 1ll * n * ( n + 1 ) * ( 2 * n + 1) ) / 6;
-	printf ( "%lld" , Sum );
+	long long sum;
+	scanf ("%d" , &n);
+	sum = (1ll * n * (n + 1) * (2 * n + 1)) / 6;
+	printf ("%lld" , sum);
+	return 0;
 }
 
 
